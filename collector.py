35ba93d333ea3097640bfe2ac66fb18ab3e1fac7
Sam Mulube <sam@umbrellium.co.uk>
2019-04-04 15:24:17 +0100

    Add some comments

--- collector.py
+++ collector.py
@@ -9,13 +9,17 @@ from datastore_client.datastore_pb2 import WriteRequest, ReadRequest
 
 client = DatastoreClient('https://datastore.decodeproject.eu')
 
-# set start time to some point in the past (here we use 1 day ago)
-start_time = datetime.now() - timedelta(days=1)
-
 # create a read request for the policy/community of interest. Note we leave
 # `end_time` nil so we are trying to read everything up until "now"
 rr = ReadRequest()
+
+# the community id should be passed in by some sort of configuration rather
+# than being fixed
 rr.community_id = '22ba20bf-4675-4d3a-9bcf-612b7db7a267'
+
+# set start time to some point in the past (here we use 1 day ago but could be
+# whatever interval the collector requires)
+start_time = datetime.now() - timedelta(days=1)
 rr.start_time.FromDatetime(start_time)
 
 # make the first request
@@ -28,7 +32,8 @@ with open('decrypt.lua') as file:
 # create decryption keys - this should be replaced with the private key Rohit is using
 keys = '{ "community_seckey": "D19GsDTGjLBX23J281SNpXWUdu+oL6hdAJ0Zh6IrRHA=" }'
 
-
+# decrypt attempts to decrypt a chunk of data using zenroom, printing out the
+# decryted values
 def decrypt(data):
     result = zenroom.execute(script.encode(), keys=keys.encode(), data=data.encode(), verbosity=1)
     print(result.decode('ascii'))
